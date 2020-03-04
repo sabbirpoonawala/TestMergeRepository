5c450efaed59c455cccf047ddec6d618fbfca2e5
sabbir.poonawala <sabbir.poonawala@YP1010527LT.Yash.com>
2020-03-04 18:50:35 +0530

    removed

--- TestPro/src/com/yash/controller/TestController.java
+++ TestPro/src/com/yash/controller/TestController.java
@@ -1,12 +1,9 @@
 package com.yash.controller;
 
 public class TestController {
-<<<<<<< HEAD
 	
 	public void x1() {}
-=======
   
   public void x2(){}
->>>>>>> branch 'master' of https://github.com/sabbirpoonawala/TestMergeRepository.git
 
 }
