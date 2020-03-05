131b3204d2c50a0ab1f759ea93587bd9f29c3c6d
sabbir.poonawala <sabbir.poonawala@YP1010527LT.Yash.com>
2020-03-05 13:01:02 +0530

    try to commit

--- TestPro/src/com/yash/controller/TestController.java
+++ TestPro/src/com/yash/controller/TestController.java
@@ -4,20 +4,7 @@ public class TestController {
 	
 	public void x1() {
 		
-		System.out.println("--line 2--");
-	    System.out.println("--line 1--");
-	    
-	    System.out.println("--line 4--");
-	    
-	    System.out.println("--line 6--");
-	    //new line
-	    System.out.println("--line 8--");
-	    System.out.println("--");
-	System.out.println("--line 7--");
-	System.out.println("--line 9--");
-	    System.out.println("---");
-	    System.out.println("--modified--");
-		System.out.println("--line 10--");
+    System.out.println("let's see if it works");
 	
 	}
 
