7af76fe32b81dfc93ea0e508376ba9a472b20a09
sabbirpoonawala <47884983+sabbirpoonawala@users.noreply.github.com>
2020-03-05 12:58:27 +0530

    it works added

--- TestPro/src/com/yash/controller/TestController.java
+++ TestPro/src/com/yash/controller/TestController.java
@@ -3,21 +3,8 @@ package com.yash.controller;
 public class TestController {
 	
 	public void x1() {
-		
-		System.out.println("--line 2--");
-	    System.out.println("--line 1--");
-	    
-	    System.out.println("--line 4--");
-	    
-	    System.out.println("--line 6--");
-	    //new line
-	    System.out.println("--line 8--");
-	    System.out.println("--");
-	System.out.println("--line 7--");
-	System.out.println("--line 9--");
-	    System.out.println("---");
-	    System.out.println("--modified--");
-		System.out.println("--line 10--");
+	
+		System.out.println("--it works--");
 	
 	}
 
